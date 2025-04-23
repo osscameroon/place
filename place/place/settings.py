9169a63c44bba30c8b89a700abc073eef55b205d
Brady Fomegne <brady.fomegne@outlook.com>
2025-04-23 15:43:55 +0100

    fix ci

--- place/place/settings.py
+++ place/place/settings.py
@@ -17,7 +17,9 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(SECRET_KEY=(str, 'insecure'), DEBUG=(bool, True), ALLOWED_HOSTS=(list, []))
+env = environ.Env(
+    SECRET_KEY=(str, "insecure"), DEBUG=(bool, True), ALLOWED_HOSTS=(list, [])
+)
 # reading .env file
 environ.Env.read_env(BASE_DIR / ".env")
 
